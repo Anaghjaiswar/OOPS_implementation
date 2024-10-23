aa8ba4b36d201c236d92fa3181fdda26c42fec5c
Anaghjaiswar <jaiswaranagh@gmail.com>
2024-10-24 00:26:04 +0530

    inheritance and polymorphism

--- oops.py
+++ oops.py
@@ -7,9 +7,9 @@
 
 class Car :
     def __init__(self,brand,year,type):
-        self.brand = brand # instance variable
+        self.brand = brand      # instance variable
         self.year = int(year)   # instance variable
-        self.type = type   # instance variable
+        self.type = type        # instance variable
 
     def display_info(self):  #method
         print (f"car: {self.brand}, {self.year}, {self.type}")
@@ -35,7 +35,7 @@ Car.display_info(car_1)
 # classmethods - woh method/function jo ki keval class me hi bound rahte hai and not objects of the class 
 # declared with @classmethod decorator and take cls as first parameter
 
-# staticmethods - inko class aur object dono se hi koi matlab nahi hai they are normal functions but belong to class namespace
+# staticmethods - inko class aur object dono se hi koi matlab nahi hai, they are normal functions but belong to class namespace
 
 
 class Student:
@@ -79,6 +79,56 @@ print(Calculator.mul(23,6))
 print(Calculator.Div(23,6))
 
     
+# INHERITANCE
+# it allows one class(child class) to inherit attributes and methods from another class(parent class).
+
+# POLYMORPHISM
+# isme same methods ki different implementation ho sakti hai different classes me 
+# used in context of inheritance
+
+class Employee:  # Employee class
+    def __init__(self,fname,lname,salary):
+        self.fname = fname
+        self.lname = lname
+        self.salary = salary
+
+    def work (self):
+        return f"{self.fname} {self.lname} is working."
+    
+    def get_salary(self):
+        return f"{self.fname} {self.lname}'s salray is {self.salary}"
+    
+
+#(Child class) inheriting from Employee
+class Manager(Employee):
+    def __init__(self,fname,lname,salary,department):
+        super().__init__(fname,lname,salary)
+        self.department = department
+
+    # Polymorphism: Overriding the work method
+    def work(self):
+        return f"{self.fname} {self.lname} is managing the {self.department} department"
+
+
+# polymorphism
+def employee_work(employee):
+    print(employee.work())
 
 
 
+# employee instance
+emp_1 = Employee("Anagh","jaiswar",60000)
+# Manager object (which inherits from employee)
+mgr_1 = Manager("pradeep","Saxena",61000,"sales")
+
+
+print(mgr_1.get_salary())
+print(mgr_1.department)
+
+
+# using polymorphism
+employee_work(emp_1)
+employee_work(mgr_1)
+
+# When employee_work() is called, it takes either an Employee or a Manager object and 
+# calls their respective work() method based on the actual type of the object. 
